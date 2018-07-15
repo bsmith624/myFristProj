39a75a86f1d121b3e35fc8f0a4569f383b74b634
Brian Smith <Brian@Brians-MacBook-Pro.local>
2018-07-15 11:33:33 -0400

    this is my first commit

---
+++ greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Brian Smith";
+document.body.innerHTML = greeter.(user);
---
+++ greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+let user = "Brian Smith";
+
+document.body.innerHTML = greeter.(user);
